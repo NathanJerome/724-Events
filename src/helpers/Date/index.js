860156d3e8337875b00317f7bba38b251fa677fb
Krakou <krakoukasse13@gmail.com>
2024-01-12 13:32:18 +0100

    all componant debugged

---
+++ src/helpers/Date/index.js
@@ -0,0 +1,18 @@
+// Changement des valeurs
+export const MONTHS = {
+  0: "janvier",
+  1: "février",
+  2: "mars",
+  3: "avril",
+  4: "mai",
+  5: "juin",
+  6: "juillet",
+  7: "août",
+  8: "septembre",
+  9: "octobre",
+  10: "novembre",
+  11: "décembre",
+};
+
+export const getMonth = (date) => MONTHS[date.getMonth()];
+// export const getMonth = (date) => console.log(MONTHS[date.getMonth()+1]);
